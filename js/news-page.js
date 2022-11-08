e926be912c1f6ed0e379f194b0bbd60c5b582064
daatoo <davit.cheishvili.06@gmail.com>
2022-11-08 18:44:44 +0400

    first commit

---
+++ js/news-page.js
@@ -0,0 +1,22 @@
+const acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    let active = this.classList.toggle("active");
+    let button = this.children[0];
+    console.log(button);
+    let panel = this.nextElementSibling;
+    let svg = this.children[0].childNodes[4];
+
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+      svg.classList.remove("rotate-180");
+      button.style.borderBottomStyle = "solid";
+    } else {
+      panel.style.display = "block";
+      svg.classList.add("rotate-180");
+      button.style.borderBottomStyle = "none";
+    }
+  });
+}
